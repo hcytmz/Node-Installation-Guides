f70f980367ad14fe55fd68b2e06488ea1c0890aa
haciyatmaz <35812219+hcytmz@users.noreply.github.com>
2023-01-15 02:05:39 +0300

    Update and rename upgrade to upgrade.sh

---
+++ Wormholes/upgrade.sh
@@ -0,0 +1,22 @@
+### :green_square: Upgrade the process of node
+1.  Conduct the command as follows and upgrade the node.
+```bash
+wget -O wormholes_install.sh https://docker.wormholes.com/wormholes_install.sh && sudo bash wormholes_install.sh
+```
+2.  When the figure is displayed below, the node has upgraded successfully.
+![image](https://user-images.githubusercontent.com/35812219/212500761-cc29935d-5022-4367-8d87-2bfae8932bfc.png)
+
+3.  Conduct the command as follows, check whether the Wormholes container is normally running or not and if it Shows “UP,” which means yes.
+```bash
+sudo docker ps -a
+```
+4.  If you want to monitor node operation in real time, you can use the monitoring script.
+```bash
+wget -O monitor.sh https://raw.githubusercontent.com/hcytmz/Testnet-Guides/main/Wormholes/monitor.sh && sudo bash monitor.sh
+```
+![image](https://user-images.githubusercontent.com/35812219/212500614-f33a03eb-dccb-42ee-8932-5b4e1f849cca.png)
+
+5.  If you waited for a while and couldn't find a peer, execute the following command.
+```bash
+docker restart wormholes
+```
